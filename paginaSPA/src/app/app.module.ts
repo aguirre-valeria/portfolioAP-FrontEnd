5b62e4fc9c52e4995518a683c155b8e5cbe1323a
Valeria Aguirre <102261096+aguirre-valeria@users.noreply.github.com>
2022-10-23 16:12:51 -0300

    creation of component folders

--- paginaSPA/src/app/app.module.ts
+++ paginaSPA/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutMeComponent,
+    FooterComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
