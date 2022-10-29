03cae1ed1e06e9ff6f12805bdf0626ec0e5d7b45
Valeria Aguirre <102261096+aguirre-valeria@users.noreply.github.com>
2022-10-29 01:07:41 -0300

    refactor: user model

--- paginaSPA/src/app/models/user.model.ts
+++ paginaSPA/src/app/models/user.model.ts
@@ -1,11 +1,21 @@
+import { Experiencie } from './experiencie.model';
+import { Education } from './education.model';
+import { Project } from './project.model';
+import { Skill } from './skill.model';
+
 export interface User {
     id: number;
     name: string;
     surname: string;
+    email: String;
     titleProfession: string;
     description: string;
     urlPhoto: string;
     urlBanner: string;
     urlGitHub: string;
     urlLinkedIn: string;
+    experiencies: Array<Experiencie>;
+    educations: Array<Education>;
+    skills: Array<Skill>;
+    projects: Array<Project>;
 }
