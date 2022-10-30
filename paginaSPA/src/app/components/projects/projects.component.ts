4e0fd78842bc6205ce806fb074e978cfe6e6363d
Valeria Aguirre <102261096+aguirre-valeria@users.noreply.github.com>
2022-10-30 07:10:30 -0300

    feat: functional add, edit and delete buttons

--- paginaSPA/src/app/components/projects/projects.component.ts
+++ paginaSPA/src/app/components/projects/projects.component.ts
@@ -1,4 +1,9 @@
+import { Project } from './../../models/project.model';
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-projects',
@@ -6,10 +11,94 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
-
-  constructor() { }
+  public user? : User | undefined;
+  public projects : User["projects"] = [];
+  public addProjects: Project | any;
+  public editProject : Project | undefined;
+  public deleteProject : Project | undefined;
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.getUser();
+    this.getProject();
   }
 
+  public getUser(): void {
+    this.userService.getUser().subscribe( {
+      next: (response: User) => {
+        this.user = response;
+      },
+      error:(error:HttpErrorResponse) => {
+        alert(error.message);
+      }
+    })
+  }
+
+  public getProject(): void {
+    this.userService.getUser().subscribe({
+      next: (response: User) => {
+        this.projects = Object.values(response.projects);
+      },
+      error: (error: HttpErrorResponse) => {
+        console.log('error');
+      },
+    });
+  }
+
+  public openModal(mode: string, project?: Project) : void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if(mode === 'add') {
+      button.setAttribute('data-target', '#addProjectModal');
+    } else if(mode === 'edit') {
+      this.editProject = project;
+      button.setAttribute('data-target', '#editProjectModal');
+    } else if(mode === 'delete') {
+      this.deleteProject = project;
+      button.setAttribute('data-target', '#deleteProjectModal');
+    }
+    container?.appendChild(button);
+    button.click();
+  }
+
+  public addProject(addForm: NgForm): void {
+    this.user?.projects.push(addForm.value);
+    this.userService.addProject(this.user).subscribe({
+      next: (response: User) => {
+        this.getProject();
+        addForm.reset();
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+        addForm.reset();
+      }
+    })
+  }  
+
+  public updateProject(project: Project): void {
+    this.editProject = project;
+    this.userService.updateProject(project).subscribe({
+      next: (response: Project) => {
+        this.getProject();
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    })
+  }
+
+  public onDeleteProject(idProj: number): void {
+    this.userService.deleteProject(idProj).subscribe({
+      next: (response: void) => {
+        alert("El proyecto ha sido eliminado.");
+        this.getProject();
+      },
+      error: (error: HttpErrorResponse) => {
+        this.getProject();
+      },
+    });
+  }
 }
--- paginaSPA/src/app/services/user.service.ts
+++ paginaSPA/src/app/services/user.service.ts
@@ -1,3 +1,4 @@
+import { Project } from 'src/app/models/project.model';
 import { environment } from 'src/environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
@@ -17,7 +18,28 @@ export class UserService {
     return this.http.get<User>(`${this.apiServerUrl}/user/id/2`);
   }
 
+  public getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiServerUrl}/user/all`);
+  }
+
   public updateUser(user: User):Observable<User> {
     return this.http.put<User>(`${this.apiServerUrl}/user/update`, user);
+  }
+
+  //PROJECTS
+  public getProjects(): Observable<User["projects"]> {
+    return this.http.get<User["projects"]>(`${this.apiServerUrl}/user/id/2`);
+  }
+
+  public addProject(project?: User): Observable<User> {
+    return this.http.put<User>(`${this.apiServerUrl}/user/update`, project);
+  }
+
+  public updateProject(project: Project):Observable<Project> {
+    return this.http.put<Project>(`${this.apiServerUrl}/project/update`, project);
+  }
+
+  public deleteProject(idProj: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/project/delete/${idProj}`);
   }
 }
